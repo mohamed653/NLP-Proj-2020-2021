1c173c88e6368cb2588ed03c24a5e5b0831ecd3b
mohamed hamed <mohamedhamed3343@gmail.com>
2020-12-21 03:18:01 +0200

    adding detialed sentiment analysis with bar graph

--- main_nltk.py
+++ main_nltk.py
@@ -1,5 +1,10 @@
 import string
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import matplotlib.pyplot as plt
+
 
 # NORMLIZATION
 
@@ -11,6 +16,15 @@ lower_case = text.lower()
 
 # 3- remove punctuation
 cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
+
+# 4- tokenization  (for the detailed sentiment analysis)
+tokenized_words =word_tokenize(cleaned_text, "english")
+
+# 5- remove stopwords from the tokenized words (for the detailed sentiment analysis)
+final_words = []
+for word in tokenized_words:
+    if word not in stopwords.words("english"):
+        final_words.append(word)
 
 
 # ALGORITHM
@@ -24,5 +38,25 @@ def sentiment_analyze(sentiment_text):
         print("Positive Sentiment")
     else:
         print("Neutral Sentiment")
-
 sentiment_analyze(cleaned_text)
+
+# normalize emotion.txt file -->(considered as a corpus)
+# then find if any word from the input_text is in this the emotion.txt and store (the emotion of that word)
+#  in the emotion_list
+# then count each emotion using a Counter
+emotion_list = []
+with open('emotion.txt','r') as file:
+    for line in file:
+        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+        word, emotion = clear_line.split(':')
+        if word in final_words:
+            emotion_list.append(emotion)
+counted_emotions = Counter(emotion_list)
+print(counted_emotions)
+
+# using matplotlib library to show the graph of the detailed sentiment analysis
+fig, ax1 = plt.subplots()
+ax1.bar(counted_emotions.keys(), counted_emotions.values())
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
